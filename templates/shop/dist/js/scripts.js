b8f623822edbcef0cd37c8bda7571cccb1a2377a
Mai Thành Duy An <tiachop0102@gmail.com>
2021-05-16 18:14:50 +0900

    [Templates] Create Shop template.

---
+++ templates/shop/dist/js/scripts.js
@@ -0,0 +1,6 @@
+/*!
+* Start Bootstrap - undefined v1.0.0 (undefined)
+* Copyright 2013-2021 Mai Thành Duy An
+* Licensed under MIT (https://github.com/StartBootstrap/shop-template/blob/master/LICENSE)
+*/
+// scripts
